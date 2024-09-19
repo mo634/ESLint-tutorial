74731d14c446ea033bf78af4504b81637d60fcd8
MohamedMostafa <83365873+mo634@users.noreply.github.com>
2024-09-19 16:55:13 +0300

    1.understand config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,8 +3,17 @@ import pluginJs from "@eslint/js";
 
 
 export default [
+
+  // to match .js files & detect type of language
   {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
+
+
+  // determine env that  run code 
   {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+
+  //  recommended rules 
+  // pluginJs.configs.recommended,
+
+
 
 ];
---
+++ main.js
@@ -0,0 +1,13 @@
+function isPostive (number){
+    const  x=0;
+    if (number > 0) {
+return true
+    }
+
+}
+
+console.log(isPostive(10))
+
+
+
+
